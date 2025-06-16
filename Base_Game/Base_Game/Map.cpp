2df5223f1e416c4b96c32e0e49f5ee82dbc8d9c6
dlckdgh0414 <ci5976547@gmail.com>
2025-06-16 23:16:00 +0900

    feat : Gimmick selection

--- Base_Game/Base_Game/Map.cpp
+++ Base_Game/Base_Game/Map.cpp
@@ -1,35 +1,35 @@
 #include "Map.h"
 
-void Map::LoadStage(char gameMap[MAP_HEIGHT][MAP_WIDTH])
-{
-	std::ifstream mapFile("Stage1.txt");
-
-	if (mapFile.is_open())
-	{
-		for (int i = 0; i < MAP_HEIGHT; i++)
-		{
-			mapFile.getline(gameMap[i], MAP_WIDTH);
-		}
-		mapFile.close();
-		return;
-	}
-	else
-	{
-		cout << "맵 파일 초기화 실패" << endl;
-	}
-}
-
-void Map::MapRender(char gameMap[MAP_HEIGHT][MAP_WIDTH])
-{
-	for (int i = 0; i < MAP_HEIGHT; ++i)
-	{
-		for (int j = 0; j < MAP_WIDTH; ++j)
-		{
-			if (gameMap[i][j] == (char)Tile::WALL)
-				cout << "■";
-			else if (gameMap[i][j] == (char)Tile::ROAD)
-				cout << "  ";
-		}
-		cout << endl;
-	}
-}
+//void Map::LoadStage(char gameMap[MAP_HEIGHT][MAP_WIDTH])
+//{
+//	std::ifstream mapFile("Stage1.txt");
+//
+//	if (mapFile.is_open())
+//	{
+//		for (int i = 0; i < MAP_HEIGHT; i++)
+//		{
+//			mapFile.getline(gameMap[i], MAP_WIDTH);
+//		}
+//		mapFile.close();
+//		return;
+//	}
+//	else
+//	{
+//		cout << "맵 파일 초기화 실패" << endl;
+//	}
+//}
+//
+//void Map::MapRender(char gameMap[MAP_HEIGHT][MAP_WIDTH])
+//{
+//	for (int i = 0; i < MAP_HEIGHT; ++i)
+//	{
+//		for (int j = 0; j < MAP_WIDTH; ++j)
+//		{
+//			if (gameMap[i][j] == (char)Tile::WALL)
+//				cout << "■";
+//			else if (gameMap[i][j] == (char)Tile::ROAD)
+//				cout << "  ";
+//		}
+//		cout << endl;
+//	}
+//}
