3adb044aeb3d6c8ff76c1712f20e3c797d0f70d2
ruddnjs13 <ruddnjs013@gmail.com>
2025-06-19 22:36:09 +0900

    feat : wordGimmickRender

--- Base_Game/Base_Game/WordGimmick.cpp
+++ Base_Game/Base_Game/WordGimmick.cpp
@@ -2,51 +2,33 @@
 
 void WordGimmick::Init()
 {
-    //char keyChar = 'A';
-    //vector<wstring> temp;
-    //wstring line;
+    char keyChar = 'A';
+    vector<string> temp;
+    string line;
 
-    //std::wifstream file("Ascii_Alphabet.txt");
+    std::ifstream file("Alphabet.txt");
 
-    //if (file.is_open())
-    //{
-    //    while (getline(file, line))
-    //    {
-    //        if (line.empty())
-    //        {
-    //            if (!temp.empty())
-    //            {
-    //                asciiMap.insert({ keyChar, temp });
-    //                temp.clear();
-    //                keyChar++;
-    //            }
-    //        }
-    //        else
-    //        {
-    //            temp.push_back(line);
-    //        }
-    //    }
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            if (line.empty())
+            {
+                if (!temp.empty())
+                {
+                    asciiMap.insert({ keyChar, temp });
+                    temp.clear();
+                    keyChar++;
+                }
+            }
+            else
+            {
+                temp.push_back(line);
+            }
+        }
 
-    //    // 마지막 글자 저장 누락 방지
-    //    if (!temp.empty())
-    //    {
-    //        asciiMap.insert({ keyChar, temp });
-    //    }
-    //}
-    //else
-    //{
-    //    cout << "알파벳 아스키 파일 초기화 실패" << endl;
-    //}
-
-    vector<wstring> temp;
-
-    temp = { L" █████╗   ",
-        L"██╔══██╗", 
-        L"███████║    ", 
-        L"██╔══██║    ",
-        L"██║  ██║    ",
-        L"╚═╝  ╚═╝  " };
-    asciiMap.insert({ 'A', temp });
+        
+    }
 }
 
 void WordGimmick::Interact(char btnType)
@@ -56,19 +38,13 @@ void WordGimmick::Interact(char btnType)
 
 void WordGimmick::GimmickRender()
 {
-			int coutmode = _setmode(_fileno(stdout), _O_U16TEXT);
-
 
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < WORD_HEIGHT; ++j)
 		{
-
 			Gotoxy(start_x + (WORD_WIDTH * i), start_y + j);
-			wcout << asciiMap[word[i]][j] << '\n';
-
+			cout << asciiMap[word[i]][j] << '\n';
 		}
-	}
-	
-			int wcoutmode = _setmode(_fileno(stdout), coutmode);
+    }
 }
