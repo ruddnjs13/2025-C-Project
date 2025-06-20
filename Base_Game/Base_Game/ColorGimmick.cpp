cc7bed3ddebd2cde864d31b9e664b362ae7e12d5
dlckdgh0414 <ci5976547@gmail.com>
2025-06-20 11:59:12 +0900

    feat : StageSlectAndPlayerWinCheck

--- Base_Game/Base_Game/ColorGimmick.cpp
+++ Base_Game/Base_Game/ColorGimmick.cpp
@@ -4,7 +4,7 @@ void ColorGimmick::Init()
 {
 	slotIdx = 0;
 	for (int i = 0; i < 5; i++) {
-		submitArr[i] = COLOR::GRAY;
+		ColorSetArr[i] = COLOR::GRAY;
 	}
 
 	start_x = GetConsoleResolution().X / 4 +5;
@@ -12,41 +12,42 @@ void ColorGimmick::Init()
 
 void ColorGimmick::Interact(char btnType)
 {
+
 	switch (btnType)
 	{
 	case '2':
-		submitArr[slotIdx] = COLOR::RED;
-		colorGimickTile[slotIdx] = ColorGimickTile::Red;
+		ColorSetArr[slotIdx] = COLOR::RED;
+		submitArr[slotIdx] = ColorGimickTile::Red;
 		slotIdx++;
 		break;
 	case '3':
-		submitArr[slotIdx] = COLOR::GREEN;
-		colorGimickTile[slotIdx] = ColorGimickTile::Green;
+		ColorSetArr[slotIdx] = COLOR::GREEN;
+		submitArr[slotIdx] = ColorGimickTile::Green;
 		slotIdx++;
 		break;
 	case '4':
-		submitArr[slotIdx] = COLOR::BLUE;
-		colorGimickTile[slotIdx] = ColorGimickTile::Blue;
+		ColorSetArr[slotIdx] = COLOR::BLUE;
+		submitArr[slotIdx] = ColorGimickTile::Blue;
 		slotIdx++;
 		break;
 	case '5':
-		submitArr[slotIdx] = COLOR::YELLOW;
-		colorGimickTile[slotIdx] = ColorGimickTile::Yellow;
+		ColorSetArr[slotIdx] = COLOR::YELLOW;
+		submitArr[slotIdx] = ColorGimickTile::Yellow;
 		slotIdx++;
 		break;
 	case '6':
-		submitArr[slotIdx] = COLOR::WHITE;
-		colorGimickTile[slotIdx] = ColorGimickTile::White;
+		ColorSetArr[slotIdx] = COLOR::WHITE;
+		submitArr[slotIdx] = ColorGimickTile::White;
 		slotIdx++;
 		break;
 	case '7':
-		submitArr[slotIdx] = COLOR::GRAY;;
-		colorGimickTile[slotIdx] = ColorGimickTile::Gray;
+		ColorSetArr[slotIdx] = COLOR::GRAY;;
+		submitArr[slotIdx] = ColorGimickTile::Gray;
 		slotIdx++;
 		break;
 	case '8':
-		submitArr[slotIdx] = COLOR::MINT;;
-		colorGimickTile[slotIdx] = ColorGimickTile::Mint;
+		ColorSetArr[slotIdx] = COLOR::MINT;;
+		submitArr[slotIdx] = ColorGimickTile::Mint;
 		slotIdx++;
 		break;
 	}
@@ -59,7 +60,7 @@ void ColorGimmick::GimmickRender()
 	{
 		for (int j = 0; j < Color_HEIGHT; ++j)
 		{
-			SetColor(submitArr[i], COLOR::BLACK);
+			SetColor(ColorSetArr[i], COLOR::BLACK);
   		    Gotoxy(start_x + (Color_WIDTH * i), start_y + j);
 			cout << "¡á¡á¡á¡á¡á¡á" << "\n";
 
---
+++ Base_Game/Base_Game/PlayerWinCheck.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+#include <string>
+#include "Console.h"
+using std::string;
+class PlayerWinCheck
+{
+public:
+	int player1Win = 0;
+	int player2Win = 0;
+	bool isPlayer1Win = false;
+	bool isPlayer2Win = false;
+
+public:
+	void PlayerWinChecker(int player1Win,int Player2Win);
+	void PlayerWinCheckRender();
+};
+
